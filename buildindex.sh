f6da7407f9be7447c1e7040aa91f8305523e0d5a
Andrey Petrov <andrey.petrov@shazow.net>
2023-02-09 11:16:21 -0600

    Add nix environment and indexer

---
+++ buildindex.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i sh shell.nix
+
+echo "{"
+leading=""
+while read line; do
+    sighash=$(cast sig "$line" 2> /dev/null)
+    if [[ $? == 1 ]]; then
+        continue # Couldn't parse, skip
+    fi
+    echo -e -n "${leading}\t\"${sighash}\": \"$line\""
+    leading=",\n"
+done
+echo ""
+echo "}"
---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    foundry.url = "github:shazow/foundry.nix/monthly"; # Use monthly branch for permanent releases
+  };
+
+  outputs = { self, nixpkgs, utils, foundry }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ foundry.overlay ];
+        };
+      in {
+
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+            foundry-bin
+          ];
+
+          # Decorative prompt override so we know when we're in a dev shell
+          shellHook = ''
+            export PS1="[dev] $PS1"
+          '';
+        };
+      });
+}
